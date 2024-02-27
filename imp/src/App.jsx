0341b49afaa28d5855880516a69a7566b4882095
mateussiilva <mateuspython3010@gmail.com>
2024-02-27 18:52:07 -0300

    Testando o front com react

---
+++ imp/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from "./components/Footer.jsx"
+
+function App() {
+  return (
+    <>
+      <footer>
+        <form action="post">
+          <div><input type="email" name="email" id="iemail" /></div>
+        </form>
+      </footer>
+    </>
+  )
+};
+
+export default App;
---
+++ imp/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+function Footer(method) {
+    return (
+        <footer>
+            <form action={{method}}>
+                <input type="text" />
+            </form>
+        </footer>
+    )
+}
+
+
+export default  Footer;
